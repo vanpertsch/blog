55bf9f46ab218cbe606a5aba7a0053281fde79c0
Vanessa Pertsch <pertsch@kontrollfeld.de>
2021-08-03 19:40:05 +0200

    First commit

---
+++ models/article.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify')
+const marked = require('marked')
+const createDOMPurify = require("dompurify");
+const { JSDOM } = require("jsdom");
+
+const window = new JSDOM("").window;
+const DOMPurify = createDOMPurify(window);
+
+const articleSchema = new mongoose.Schema({
+
+title: {
+  type: String,
+  required: true
+},
+description: {
+  type: String
+},
+markdown: {
+  type: String,
+  required: true
+},
+createdAt:{
+  type: Date,
+  default: Date.now
+},
+slug: {
+  type: String,
+  required:true,
+  unique: true
+},
+sanitizedHtml: {
+  type: String,
+  required: true
+}
+
+})
+
+articleSchema.pre('validate', function(next){
+  if(this.title){
+    this.slug = slugify(this.title, {
+      lower: true,
+      strict: true
+    })
+  }
+  if (this.markdown) {
+    this.sanitizedHtml = DOMPurify.sanitize(marked(this.markdown));
+  }
+  next()
+})
+
+module.exports = mongoose.model('Article', articleSchema)
